533d9c4fdca1251bfc2fd90ccde0ece54a9ad338
GarethW <weirdybeardy@gmail.com>
2024-12-07 16:49:19 +0000

    Day2 part2 solved (Python) with a little help from Reddit :)

--- day2/day2.py
+++ day2/day2.py
@@ -1,38 +1,29 @@
 #!/usr/bin/python3
 import sys
 
-debug = True
+debug = False
 
 file = sys.argv[1]
-# sep = '   '
 f = open(file, 'r')
 
 safeTotal = 0
 unSafeTotal = 0
 
-for line in f.readlines():
-    # Need to catch cases where removing the first number would result in a positive result
-    unSafe = False
-    if debug:
-        print("Analyzing %s" % line)
-    splitLine = (line.split(' '))
-    increasing = None
-    increaseFlips = 0
-    cur = int(splitLine[0])
+def checkViolations(splitLine, violationLevel):
+    # Set up loop vars
+    increasing = None # boolean to track whether previous numbers are increasing/decreasing
     safeLevel = 0
+    cur = int(splitLine[0]) # set first instance as cur, then loop over [1:]
     for num in splitLine[1:]:
-        safeLevelChange = False
+        safeLevelChange = False 
         if debug:
             print('safeLevel: %d' % safeLevel)
-        if(safeLevel > 1):
-            break
         new = int(num)
         if debug:
             print("Comparing %d to %d, increasing: %s" % (cur, new, increasing))
         if (abs(new - cur) > 3 or abs(new - cur) == 0):
             safeLevel += 1
             safeLevelChange = True
-            
         if ((new - cur) > 0):
             if (increasing == False):
                 safeLevel += 1
@@ -41,18 +32,48 @@ for line in f.readlines():
                 increasing = True
         if ((new - cur) < 0):
             if (increasing == True):
-               safeLevel += 1
-               safeLevelChange = True
+                safeLevel += 1
+                safeLevelChange = True
             else:
                 increasing = False
-        if (safeLevelChange == False):
+        if (safeLevelChange == False): # If safe level changed, keep last cur
             cur = new
-    if (safeLevel > 1):
+    if (safeLevel > violationLevel):
         if debug:
-            print("Declaring %s unsafe" % line.rstrip())
-        unSafeTotal += 1
+            print("Declaring %s unsafe\n" % line.rstrip())
+        return 1
     else:
-        safeTotal += 1
+        if debug:
+            print("Declaring %s Safe\n" % line.rstrip())
+        return 0
 
+
+
+for line in f.readlines():
+    if debug:
+        print("Analyzing %s" % line)
+    splitLine = (line.split(' '))
+    if (checkViolations(splitLine, 1) == 0):
+        if debug:
+            print("Safe on FIRST pass")
+        safeTotal += 1
+    else:
+        safe = False
+        for i in range(0,(len(splitLine) - 1)):
+            checkLine = splitLine.copy()
+            checkLine.pop(i)
+            if debug:
+               print("checkline: ", checkLine)
+            if(checkViolations(checkLine, 0) == 0):
+                if debug:
+                   print("Safe on pass: %d" % i)
+                safe = True
+                break
+        if (safe == False):
+            unSafeTotal +=1
+            if debug: 
+               print("Declared UnSafe")
+        else:
+            safeTotal += 1
 
 print("Safe: %d, Unsafe: %d" % (safeTotal, unSafeTotal))
