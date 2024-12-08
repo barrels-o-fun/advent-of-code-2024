29609f639a6d4ae9664ff03ce0db16876f7f78e2
GarethW <weirdybeardy@gmail.com>
2024-12-08 11:45:15 +0000

    Day4 Pt2(Python) 2 guesses this time.

--- day4/day4.py
+++ day4/day4.py
@@ -17,7 +17,7 @@ def findChar (c, arr):
     known_pos = [] 
     row = 0
     col = 0
-    for line in puzzleArr:
+    for line in arr:
         col = 0
         for char in line:
             if char == c:
@@ -49,26 +49,93 @@ def searchXmas (arrX, arrM, arrA, arrS, x_dir, y_dir):
    
     return found_full
 
+def searchCrossmas (arrA, arrM, arrS):
+    found_full = 0
+    for a in arrA:
+        topL_oob: False
+        topR_oob = False
+        botL_oob = False
+        botR_oob = False
+        found_m = 0
+        found_s = 0
+        row = a[0]
+        col = a[1]
+        # print("Checking: ", m)
+        # We want to find "Exactly 2 M's and 2 S's"
+        # check top left
+        row -= 1
+        col -= 1
+        if arrM.count([row, col]):
+            found_m += 1
+            botR_oob = True
+        # check top right
+        row += 2
+        if arrM.count([row, col]):
+            found_m += 1
+            botL_oob = True
+        # check bottom right
+        col += 2
+        if (arrM.count([row, col]) and botR_oob == False):
+            found_m += 1
+        # check bottom left
+        row -= 2
+        if (arrM.count([row, col]) and botL_oob == False):
+            found_m += 1
+        
+        # Only check S's if we have found exactly 2 M's
+        if found_m == 2:
+            # reset pos of 'A' before checking for S's
+            row = a[0]
+            col = a[1]
+            # check top left
+            row -= 1
+            col -= 1
+            if arrS.count([row, col]):
+                found_s += 1
+            # check top right
+            row += 2
+            if arrS.count([row, col]):
+                found_s += 1
+            # check bottom right
+            col += 2
+            if arrS.count([row, col]):
+                found_s += 1
+            # check bottom left
+            row -= 2
+            if arrS.count([row, col]):
+                found_s += 1
+            # print("m: ", found_m, "s: ", found_s)
+            if found_m == 2 and found_s == 2:
+                found_full += 1
+                if debug:
+                    print("Found X-MAS at A-Pos: ", a[0], a[1])
+    return found_full
 
-known_x_pos = findChar('X', puzzleArr)
+
+    
+
+
+# known_x_pos = findChar('X', puzzleArr)
 known_m_pos = findChar('M', puzzleArr)
 known_a_pos = findChar('A', puzzleArr)
 known_s_pos = findChar('S', puzzleArr)
 
-if debug:
-    print('X: ', known_x_pos)
-    print('M: ', known_m_pos)
-    print('A: ', known_a_pos)
-    print('S: ', known_s_pos)
+# if debug:
+#     # print('X: ', known_x_pos)
+#     print('M: ', known_m_pos)
+#     print('A: ', known_a_pos)
+#     print('S: ', known_s_pos)
 
-total = searchXmas(known_x_pos,known_m_pos,known_a_pos,known_s_pos, 'right', '')
-total += searchXmas(known_x_pos,known_m_pos,known_a_pos,known_s_pos, 'left', '')
-total += searchXmas(known_x_pos,known_m_pos,known_a_pos,known_s_pos, '', 'down')
-total += searchXmas(known_x_pos,known_m_pos,known_a_pos,known_s_pos, '', 'up')
-total += searchXmas(known_x_pos,known_m_pos,known_a_pos,known_s_pos, 'left', 'up')
-total += searchXmas(known_x_pos,known_m_pos,known_a_pos,known_s_pos, 'left', 'down')
-total += searchXmas(known_x_pos,known_m_pos,known_a_pos,known_s_pos, 'right', 'up')
-total += searchXmas(known_x_pos,known_m_pos,known_a_pos,known_s_pos, 'right', 'down')
+total = searchCrossmas(known_a_pos, known_m_pos, known_s_pos)
+
+# total = searchXmas(known_x_pos,known_m_pos,known_a_pos,known_s_pos, 'right', '')
+# total += searchXmas(known_x_pos,known_m_pos,known_a_pos,known_s_pos, 'left', '')
+# total += searchXmas(known_x_pos,known_m_pos,known_a_pos,known_s_pos, '', 'down')
+# total += searchXmas(known_x_pos,known_m_pos,known_a_pos,known_s_pos, '', 'up')
+# total += searchXmas(known_x_pos,known_m_pos,known_a_pos,known_s_pos, 'left', 'up')
+# total += searchXmas(known_x_pos,known_m_pos,known_a_pos,known_s_pos, 'left', 'down')
+# total += searchXmas(known_x_pos,known_m_pos,known_a_pos,known_s_pos, 'right', 'up')
+# total += searchXmas(known_x_pos,known_m_pos,known_a_pos,known_s_pos, 'right', 'down')
 
     # found = []
     # if known_m_pos.count([row - 1, col]):
@@ -79,7 +146,7 @@ total += searchXmas(known_x_pos,known_m_pos,known_a_pos,known_s_pos, 'right', 'd
     #             found.append([row - 3, col]) 
     #             found_left.append(found)
     
-print("Total: ", len(total))
+print("Total: ", total)
 
 # print("found_right: ", len(found_right))
 # print("found_left: ", len(found_left))
