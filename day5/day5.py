74dd3edd2202f48787298d27d12a1e7d64427942
GarethW <weirdybeardy@gmail.com>
2024-12-10 21:03:20 +0000

    moved stuff to functions, but I'm still a bit confused how this works

--- day5/day5.py
+++ day5/day5.py
@@ -21,90 +21,67 @@ for line in f:
 print("Rules: ", rules)
 print("Updates: ", updates)
 
-
-
-
 # We want to know which of the "Pages" match the rules
 
-
-
-
-# If LHS
-#   loop through list of pageNums
-#   check if we get to a or b first
-#   if get to 'a' first this is GOOD and we can break out
-#   if get to 'b' first this is BAD and we can break out
-# Loop through pageNums 
-
 GoodUpdates = []
+
+def is_a_first (a, b, numList):
+    target = b
+    found_a = False
+    found_target = False
+    for num in numList:
+        # if we get to 'a' first, we can return True
+        if found_a == True and found_target == False:
+            return True
+        # if get to target first this is BAD and we can break out
+        elif found_target == True and found_a == False:
+            return False
+        if num == a:
+            found_a = True
+        elif num == target:
+            found_target = True
+    return True
+
+def is_b_first (a, b, numList):
+    target = a
+    found_b = False
+    found_target = False
+    for num in numList:
+        if num == b:
+            found_b = True
+        elif num == target:
+            found_target = True
+        # if we get to 'a' first, we need to keep checking
+        if found_b == True:
+            continue
+        # if get to target first this is BAD and we can break out
+        elif found_target == True and found_b == False:
+            return True
+
+    return True
 
 # Loop through page number lists
 for update in updates:
     updateValid = True
-    # print("Checking update: ", update)
     # within each list loop through num
     for pageNum in update: 
         if updateValid == False:
             break
-        # num = int(num)  # I thought I had these as Ints already but ok :-D, yup I was an idiot :D
         # Check in rules for our num
         for rule in rules:
-            # check each num against available rules
-            # print("Checking rule: ", rule)
-            # print(rule)
             a, b = rule
-            # print("a: ", repr(a))
+            # check each num against available rules
             # if num in rule
             # check if num is on LHS or RHS of tuple
             # check if rule is valid
             # Checking for LHS of tuple
             if pageNum == a: # LHS of tuple
                 # print("Checking", rule, "LHS")
-                target = b
-                found_a = False
-                found_target = False
-                # print("Found num", pageNum, "as 'A' in rule: ", rule)
-                # loop through list of pageNums
-                for num in update:
-                    # print("Checking against: ", num)
-                    # check if we get to a or b first
-                    # if get to 'b' first this is BAD and we can break out
-                    if found_target == True:
-                        updateValid = False
-                        # print("Rule Bad: ", update, "Due to rule: ", rule)
-                        break
-                    # if get to 'a' first this is GOOD and we can break out
-                    elif found_a == True:
-                        continue
-                    if num == a:
-                        found_a = True
-                    elif num == target:
-                        found_target = True
-
+                if (is_a_first(a, b, update) == False):
+                    updateValid = False
             elif pageNum == b:  # RHS of tuple
-                # print("Checking", rule, "RHS")
-                target = a
-                found_b = False
-                found_target = False
-                # print("Found num", pageNum, "as 'A' in rule: ", rule)
-                # loop through list of pageNums
-                for num in update:
-                    # print("Checking against: ", num)
-                    # check if we get to a or b first
-                    # if get to 'b' first this is BAD and we can break out
-                    if found_target == True:
-                        break
-                    # if get to 'a' first this is GOOD and we can break out
-                    elif found_target == True and found_b == False:
-                        updateValid = False
-                        # print("Rule Bad: ", update, "Due to rule: ", rule)
-                        break
-                    if num == b:
-                        found_b = True
-                    elif num == target:
-                        found_target = True
-
-                # print("Found num", pageNum, "as 'B' in rule: ", rule)
+                if (is_b_first(a,b, update) == False):
+                    updateValid = False
                 
     if updateValid == False:
         print("Update Bad: ", update)
