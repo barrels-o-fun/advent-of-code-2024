e189f499df468daf93c5a27cac0f9a6102c8ec48
barrels-o-fun <weirdybeardy@gmail.com>
2024-12-15 21:20:48 +0000

    day7 p2 (not right yet)

--- day7/day7.py
+++ day7/day7.py
@@ -1,15 +1,9 @@
 #!/usr/bin/python3
 
 import sys
+import time
 
 DEBUG = False
-
-input_file = sys.argv[1]
-good_ones = 0
-part1_total = 0
-
-
-input_data = open(input_file).readlines()
 
 def add(a, b):
     if DEBUG:
@@ -21,16 +15,16 @@ def multiply(a, b):
         print("Multiplying:", a, b)
     return a * b
 
-for line in input_data:
-    total = 0
-    res, nums = line.split(": ")
-    res = int(res)
-    numsList = nums.rstrip().split(' ')
-    if DEBUG:
-        print(len(numsList), repr(numsList))
+def combine(a, b):
+    a = str(a)
+    b = str(b)
+    result = a + b
+    return result
+
+def get_combinations_binary(numsList):
     combinations = []
     numsLen = len(numsList) - 1
-    for num in range(0, (2 ** numsLen)):
+    for num in range(0, (3 ** numsLen)):
         binNum = format(num, 'b')
         if len(binNum) < numsLen:
             binNum =  ('0' * (numsLen - len(binNum))) + binNum
@@ -38,9 +32,11 @@ for line in input_data:
     
     if DEBUG:
         print("Combinations:", combinations)
-
+    return combinations
+        
+def calculate_from_combo(numsList, combinations):
     firstNum = int(numsList.pop(0))
-    for i, combo in enumerate(combinations):
+    for combo in combinations:
         # print("Checking combination:", combo)
         total = firstNum
         for j in range(0, len(numsList)):
@@ -55,25 +51,68 @@ for line in input_data:
         if DEBUG:
             print("Result", res, total)
         if total == res:
-            print("Found one!", res)
-            good_ones += 1
-            part1_total += res
-            break
-    # check if '*' gives res
+            if DEBUG:
+                print("Found one!", res)
+            return 0, res
+    # return 1 if we haven't found a good combo
+    return 1, 0
 
+def createCombinedNumberOptions(l):
+    results = []
+    results.append(l)
+    for i in range(0, (len(l) - 1)):
+        lTemp = l.copy()
+        lTemp.pop(i)
+        lTemp.pop(i)
+        num1 = l[i]
+        num2 = l[i + 1]
+        result = combine(num1, num2)
+        lTemp.insert(i, result)
+        results.append(lTemp)
+    return results
+
+
+
+##############
+# Start here #
+##############
+
+input_file = sys.argv[1]
+good_ones = 0
+part1_total = 0
+
+input_data = open(input_file).readlines()
+
+for line in input_data:
+    total = 0
+    res, nums = line.split(": ")
+    res = int(res)
+    numsList = nums.rstrip().split(' ')
+    if DEBUG:
+        print(len(numsList), repr(numsList))
+    all_combos = createCombinedNumberOptions(numsList)
+    for combo in all_combos:
+        combo_total = 0
+        # print("Checking combo: ", combo)
+        combinations = get_combinations_binary(combo)
+        result, combo_total = calculate_from_combo(combo, combinations)
+        if combo_total != 0:
+            good_ones += 1
+            part1_total += combo_total
+
+print("Total rows: ", len(input_data))
 print("Good:", good_ones)
 print("Part1:", part1_total)
 
 
-
-# if len = 2, combos are
+# if len = 2, combos are 2**2
 # + +  00
 # + *  01 
 # * +  10
 # * *  11
 
 
-# if len = 3, combos are:
+# if len = 3, combos are 3**2
 # + + + 0 0 0
 # + + * 0 0 1
 # + * +
@@ -82,6 +121,5 @@ print("Part1:", part1_total)
 # * + *
 # * * +
 # * * *
-
 
  # len 4 = 16 combinations   
