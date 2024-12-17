690c4503f57988f73e67c72b4b5689d52c9ebf6e
GarethW <weirdybeardy@gmail.com>
2024-12-17 21:18:19 +0000

    Day 1 (Go) - part2

--- day1/day1.go
+++ day1/day1.go
@@ -1,8 +1,8 @@
-// $ time ./day1
 // Part 1 Total:  1388114
+// Part 2 Total:  23529853
 
-// real    0m0.004s
-// user    0m0.004s
+// real    0m0.010s
+// user    0m0.010s
 // sys     0m0.001s
 
 package main
@@ -22,7 +22,20 @@ func check(e error) {
 	}
 }
 
+func countInstances(s []string, substr string) int {
+	counter := 0
+	for i := range s {
+		if s[i] == substr {
+			counter += 1
+		}
+	}
+	return counter
+}
+
 func main() {
+	part1_total := 0
+	part2_total := 0
+
 	// inFile := "sampleData.txt"
 	inFile := "day1.txt"
 	f, err := os.Open(inFile)
@@ -50,8 +63,6 @@ func main() {
 	sort.Strings(lhs)
 	sort.Strings(rhs)
 
-	part1_total := 0
-
 	for i := 0; i < len(lhs); i++ {
 		num1, _ := strconv.Atoi(lhs[i])
 		num2, _ := strconv.Atoi(rhs[i])
@@ -62,8 +73,17 @@ func main() {
 		part1_total += int(abs)
 	}
 
-	// fmt.Println(lhs)
-	// fmt.Println(rhs)
+	// We are counting the quantity per list (slice?) so no need to convert to Int (until we need to times it!)
+	for i := 0; i < len(lhs); i++ {
+		num := lhs[i]
+		amount := countInstances(rhs, num)
+		if amount != 0 {
+			numint, _ := strconv.Atoi(num)
+			part2_total += (amount * numint)
+		}
+	}
+
 	// fmt.Println(diff)
 	println("Part 1 Total: ", part1_total)
+	println("Part 2 Total: ", part2_total)
 }
